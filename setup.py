f405512ab9ef6bccf7a49332324d2eda5be152b7
Chandrashekar Naik <chandrunaik456@gmail.com>
2022-07-10 18:23:52 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+from numpy import require
+from setuptools import setup, find_packages
 from typing import List
 
 
 # Declaring variables for setup functions
 PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
-AUTHOR = "Chandrashekar"
+VERSION = "0.0.3"
+AUTHOR = "Chandrashekar Naik"
 DESCRIPTION = "This is the first machine learning project from the scratch"
-PACKAGES = ["housing"]
+
 REQUIREMENT_FILE_NAME ="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
 
 
 def get_requirements_list()->List[str]:
@@ -22,7 +25,11 @@ def get_requirements_list()->List[str]:
     """
 
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_file.readlines()
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
 
 setup(
@@ -30,10 +37,9 @@ name = PROJECT_NAME,
 version = VERSION,
 author= AUTHOR,
 description = DESCRIPTION,
-packages = PACKAGES,
+packages = find_packages(),
 install_requires = get_requirements_list()
-
 )
 
-if __name__ =="main":
-    print(get_requirements_list())
+
+
