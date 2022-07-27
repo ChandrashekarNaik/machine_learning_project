85398036e6b87abebcca93c94f401be9e76cd99d
Chandrashekar Naik <chandrunaik456@gmail.com>
2022-07-27 11:08:01 +0530

    data transformation completed

--- demo.py
+++ demo.py
@@ -1,20 +1,28 @@
-import logging
-import plistlib
-from housing import pipeline
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
-
+from housing.logger import logging
+from housing.config.configuration import Configuartion
+from housing.component.data_transformation import DataTransformation
 
 def main():
     try:
         pipeline = Pipeline()
         pipeline.run_pipeline()
+        data_validation_config = Configuartion().get_data_transformation_config()
+        print(data_validation_config)
+        # schema_file_path=r"C:\Users\chand\FSDS Project\machine_learning_project\config\schema.yaml"
+        # file_path=r"C:\Users\chand\FSDS Project\machine_learning_project\housing\artifact\data_ingestion\2022-07-27-10-42-13\ingested_data\train\housing.csv"
+
+        # df= DataTransformation.load_data(file_path=file_path,schema_file_path=schema_file_path)
+        # print(df.columns)
+        # print(df.dtypes)
 
     except Exception as e:
         logging.error(f"{e}")
         print(e)
 
-if __name__ == "__main__":
-    main()
 
+
+if __name__=="__main__":
+    main()
 
--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -1,24 +1,23 @@
 from collections import namedtuple
 
 
-DataIngestionConfig = namedtuple("DataIngestionConfig",
-["dataset_download_url","tgz_download_dir","raw_data_dir","ingestd_train_dir","ingestd_test_dir"])
+DataIngestionConfig=namedtuple("DataIngestionConfig",
+["dataset_download_url","tgz_download_dir","raw_data_dir","ingested_train_dir","ingested_test_dir"])
 
 
-DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path", "report_file_path", "report_page_file_path"])
-
+DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path","report_file_path","report_page_file_path"])
 
 DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_per_room",
-                                                                    "trannsformed_train_dir",
-                                                                    "transformed_test_dir",
-                                                                    "preprocessed_object_file_path"])
+                                                                   "transformed_train_dir",
+                                                                   "transformed_test_dir",
+                                                                   "preprocessed_object_file_path"])
 
 
-ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["trained_model_file_path", "base_accuracy"])
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["trained_model_file_path","base_accuracy"])
 
-ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", ["model_evaluation_file_path", "time_stamp"])
+ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", ["model_evaluation_file_path","time_stamp"])
 
-ModelPusherConfig = namedtuple("ModelPushConfig",["export_dir_path"])
+
+ModelPusherConfig = namedtuple("ModelPusherConfig", ["export_dir_path"])
 
 TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
-
